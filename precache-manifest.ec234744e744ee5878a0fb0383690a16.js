bc97810a036c2fca8656cd906a865fc09ad6ad47
Pedro Mello <pedrocmello.bsi@gmail.com>
2020-08-18 19:21:36 -0300

    Updates

---
+++ precache-manifest.ec234744e744ee5878a0fb0383690a16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1d37c6ebb351b20304a81466da74e1",
+    "url": "/pipefy-ui-clone/index.html"
+  },
+  {
+    "revision": "98f71a3732d33d1a5294",
+    "url": "/pipefy-ui-clone/static/js/2.0968a469.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pipefy-ui-clone/static/js/2.0968a469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d0f2a237409b47a066",
+    "url": "/pipefy-ui-clone/static/js/main.ec4c5928.chunk.js"
+  },
+  {
+    "revision": "01b65ef5547cd96e4280",
+    "url": "/pipefy-ui-clone/static/js/runtime-main.9c0d2507.js"
+  }
+]);
